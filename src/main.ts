0094827d6e25909f77d339bf147df4fd10c73235
xiaodongxier <xiaodongxier@gmail.com>
2024-01-22 23:24:33 +0800

    定时同步完成...

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+/*
+ * @Date: 2024-01-22 23:20:04
+ * @LastEditTime: 2024-01-22 23:21:08
+ * @Author: wangyongjie
+ * @Note:  
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).mount("#app");

bc6eb3aec009c01be0b791e2c9917c632121c7ca
xiaodongxier <xiaodongxier@gmail.com>
2024-01-22 23:00:23 +0800

    定时同步完成...

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
+<!--
+ * @Date: 2024-01-16 23:18:33
+ * @LastEditTime: 2024-01-22 22:59:22
+ * @Author: wangyongjie
+ * @Note:  
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page1</h1>
   </div>
 </template>
 

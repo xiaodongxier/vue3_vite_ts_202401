970651c3fcb95ad4ddedab76a920c99bd3842c6c
xiaodongxier <xiaodongxier@gmail.com>
2024-01-23 23:44:16 +0800

    定时同步完成...

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -2,6 +2,9 @@
   <div>
     {{ mydata.name }}-{{ mydata.age }}
     <button @click="handleClick">切换</button>
+    <hr>
+    {{ mydata.a.b.c }}
+    <button @click="handleClickC">切换</button>
 
 
   </div>
@@ -11,13 +14,22 @@
 import { reactive, ref } from 'vue'
 let mydata = ref({
   name: '张三',
-  age: 18
+  age: 18,
+  a: {
+    b: {
+      c: 9999999
+    }
+  }
 })
 
 console.log(mydata.value)
 
 function handleClick() {
   mydata.value.age  += 20
+}
+
+function handleClickC(){
+  mydata.value.a.b.c += 100
 }
 </script>
 <style lang="" scoped>

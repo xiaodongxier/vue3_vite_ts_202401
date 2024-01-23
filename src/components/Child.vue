d04014593e1b02d845451864b6baf8ba7a07d040
xiaodongxier <xiaodongxier@gmail.com>
2024-01-23 23:01:42 +0800

    定时同步完成...

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     {{ name.name }}-{{ name.age }}
-
+    <button @click="handleClick">切换</button>
 
 
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-let name = {
+import { reactive } from 'vue'
+let name = reactive({
   name: '张三',
   age: 18
+})
+
+console.log(name)
+
+function handleClick() {
+  name.age += 20
 }
+
 </script>
 <style lang="" scoped>
   

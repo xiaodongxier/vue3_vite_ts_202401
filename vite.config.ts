bf1484d3f50b743d97470f31c4ba90f2f05d24ed
xiaodongxier <xiaodongxier@gmail.com>
2024-01-23 22:40:59 +0800

    定时同步完成...

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2024-01-16 23:18:33
- * @LastEditTime: 2024-01-22 22:02:59
+ * @LastEditTime: 2024-01-23 22:39:34
  * @Author: wangyongjie
  * @Note:  
  */
@@ -10,11 +10,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // console 插件
 import TurboConsole from 'unplugin-turbo-console/vite'
+import VueSetComputedName from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    TurboConsole()
+    TurboConsole(),
+    VueSetComputedName()
   ],
   resolve: {
     alias: {

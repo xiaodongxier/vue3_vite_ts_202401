ba8ae298b6638b4167a770a8a64411bd842bdae3
xiaodongxier <xiaodongxier@gmail.com>
2024-01-24 22:01:10 +0800

    定时同步完成...

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,25 +1,20 @@
 <!--
  * @Date: 2024-01-22 23:27:13
- * @LastEditTime: 2024-01-23 22:47:47
+ * @LastEditTime: 2024-01-24 21:59:40
  * @Author: wangyongjie
  * @Note:  
 -->
 <template>
   <div class="Preson">
-    {{name}} - {{ age }} -- {{ hha }}
-    <button @click="handleClick">点击</button>
+    姓：<input type="text">
+    名：<input type="text">
+    姓名：<span>？？？？</span>
   </div>
 </template>
 
 
 <script setup lang="ts" name="Preson">
-import {ref} from 'vue'
-let name = ref("张三");
-let age = ref(18);
-function handleClick(){
-    age.value += 1
-  console.log('点击了', age);  
-}
+
 </script>
 
 

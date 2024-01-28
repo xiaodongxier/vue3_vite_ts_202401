e4066f48127bcaa4274b1322659eab0dc9d34dc3
xiaodongxier <xiaodongxier@gmail.com>
2024-01-28 21:38:06 +0800

    note-sync syncing

--- src/components/watch.vue
+++ src/components/watch.vue
@@ -1,6 +1,6 @@
 <!--
  * @Date: 2024-01-28 17:58:58
- * @LastEditTime: 2024-01-28 21:36:00
+ * @LastEditTime: 2024-01-28 21:38:06
  * @Author: wangyongjie
  * @Note:  
 -->
@@ -16,12 +16,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref , watch} from 'vue';
 const title = ref("watch相关")
 const sum = ref(0);
 function handleClick(){
   sum.value += 1
 }
 
+watch(sum, (newValue, oldValue)=> {
+  console.log("sum变化了-新值", newValue);
+  console.log("sum变化了-旧值", oldValue);
+})
 </script>
 <style lang="" scoped></style>

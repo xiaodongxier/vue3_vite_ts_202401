3d16f864f2106dc5c89f9541ab8ed31d13652803
xiaodongxier <xiaodongxier@gmail.com>
2024-01-28 15:52:26 +0800

    note-sync syncing

--- src/components/toRefs.vue
+++ src/components/toRefs.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="child-wrap">
-    {{ name }}
+    title:{{ name }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 const name = "toRefs";
 const n = ref(0);
+const info = reactive({
+  name: '张三',
+  age: 18,
+  job: {
+    j1: {
+      salary: 20
+    }
+  }
+})
 console.log(n.value);
+
 </script>
 <style lang="" scoped>
 </style>

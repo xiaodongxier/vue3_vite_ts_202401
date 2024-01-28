27c1685191bfbb1d9c3b914e739382d8e8052ffc
xiaodongxier <xiaodongxier@gmail.com>
2024-01-28 15:43:24 +0800

    note-sync syncing

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -5,7 +5,7 @@
  * @Note:  
 -->
 <template>
-  <div class="Preson">
+  <div class="child-wrap">
     姓：<input type="text" v-model="firstName">
     名：<input type="text" v-model="lasterName">
     姓名：<span>{{firstName}} - {{ lasterName }}</span>
@@ -31,9 +31,4 @@ function handleEditName(){
 
 
 <style scoped>
-.Preson {
-  background: #ccc;
-  border: 1px solid #000;
-  padding: 20px;
-}
 </style>

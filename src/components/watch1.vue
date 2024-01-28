7c666d4525e65fb431a43db5d73cf4b426143488
xiaodongxier <xiaodongxier@gmail.com>
2024-01-28 21:49:15 +0800

    note-sync syncing

---
+++ src/components/watch1.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Date: 2024-01-28 17:58:58
+ * @LastEditTime: 2024-01-28 21:45:10
+ * @Author: wangyongjie
+ * @Note:  
+-->
+<template>
+  <div class="child-wrap">
+    <h3>{{title}}</h3>
+    求和：{{ sum }}
+    <br>
+    <button @click="handleClick">点击6下停止监听</button>
+
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref , watch} from 'vue';
+const title = ref("watch相关")
+const sum = ref(0);
+function handleClick(){
+  sum.value += 1
+}
+
+const stopWatch =  watch(sum, (newValue, oldValue)=> {
+  console.log("sum变化了-新值", newValue);
+  console.log("sum变化了-旧值", oldValue);
+  if(newValue > 5){
+    stopWatch()
+    console.log("停止监听");
+    title.value = "watch相关-停止监听"
+  }
+})
+</script>
+<style lang="" scoped></style>

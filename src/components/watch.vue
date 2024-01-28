48bfd6fcaf5f79c6259be3d8bbe5f420fe0bd3f2
xiaodongxier <xiaodongxier@gmail.com>
2024-01-28 21:36:02 +0800

    note-sync syncing

--- src/components/watch.vue
+++ src/components/watch.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="child-wrap">
     <h3>{{title}}</h3>
-
+    求和：{{ sum }}
+    <br>
+    <button @click="sum++">点击</button>
 
 
   </div>
@@ -10,7 +12,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const title = ref("watch相关")
-
+const sum = ref(0);
 
 
 </script>

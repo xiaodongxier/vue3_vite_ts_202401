02ac1ee2cef250ed109140d75c1c0755b3ec132f
wangyongjie <xiaodongxier@gmail.com>
2024-12-14 00:15:43 +0800

    note-sync syncing

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <!--
  * @Date: 2024-01-19 15:38:25
- * @LastEditTime: 2024-12-12 21:20:26
+ * @LastEditTime: 2024-12-13 23:46:29
  * @Author: wangyongjie
  * @Note:  
 -->
 <template>
-  <div>{{ name }}</div>
-  <div>{{ age }}</div>
-
-  <input type="text" v-model="age">
-  <input type="text" v-model="name">
+  姓：<input type="text" v-model="firstName">
+  <hr>
+  名：<input type="text" v-model="lastName">
+  <hr>
+  姓名：{{ name }}
+  <hr>
+  {{ firstName }} - {{ lastName }}
 </template>
 
 
 <script setup lang="ts">
-import { reactive, ref, toRef, toRefs } from 'vue';
-let box = reactive({
-  name: 'wangyongjie',
-  age: 18
-})
+import { ref } from 'vue';
+let firstName = ref('王')
+let lastName = ref('先生')
 
-let { name, age } = toRefs(box);
-let newName = toRef(box, 'name')
+let name = firstName.value + lastName.value
+
 </script>

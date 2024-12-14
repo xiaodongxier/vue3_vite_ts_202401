c7a373ffe53fc7a83c6328b2314d8a0348ef9a81
wangyongjie <xiaodongxier@gmail.com>
2024-12-14 23:30:31 +0800

    note-sync syncing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!--
  * @Date: 2024-01-19 15:38:25
- * @LastEditTime: 2024-12-13 23:46:29
+ * @LastEditTime: 2024-12-14 23:19:43
  * @Author: wangyongjie
  * @Note:  
 -->
@@ -9,17 +9,37 @@
   <hr>
   名：<input type="text" v-model="lastName">
   <hr>
-  姓名：{{ name }}
-  <hr>
-  {{ firstName }} - {{ lastName }}
+  姓名：{{ name2 }}
+  <button @click="handleClickName()">直接修改姓名</button>
 </template>
 
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 let firstName = ref('王')
 let lastName = ref('先生')
 
-let name = firstName.value + lastName.value
+// 方法
+let name = function () {
+  console.log("调取了1次")
+  return firstName.value + lastName.value
+}
 
+
+// 可读，可修改
+let name2 = computed({
+  get() {
+    return firstName.value + lastName.value;
+  },
+  set(value: string) {
+    console.log("value", value)
+    const [firstName1, lastName1] = value.split("-")  // 数组解构赋值
+    firstName.value = firstName1
+    lastName.value = lastName1
+  },
+});
+
+function handleClickName() {
+  name2.value = "张-三"
+}
 </script>

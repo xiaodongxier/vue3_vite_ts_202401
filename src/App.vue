d8f1124f22c7f5bf49488548c14b70d61eb0fc25
wangyongjie <xiaodongxier@gmail.com>
2024-12-18 00:12:47 +0800

    note-sync syncing

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,27 @@
 <!--
  * @Date: 2024-01-19 15:38:25
- * @LastEditTime: 2024-12-16 09:15:40
+ * @LastEditTime: 2024-12-17 23:43:51
  * @Author: wangyongjie
  * @Note:  
 -->
 <template>
-  姓：<input type="text" v-model="firstName">
-  <hr>
-  名：<input type="text" v-model="lastName">
-  <hr>
-  姓名：{{ name2 }}
-  <button @click="handleClickName()">直接修改姓名</button>
+  <div class="parent">
+    这是父组件
+    <Child></Child>
+  </div>
+
 </template>
 
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-let firstName = ref('王')
-let lastName = ref('先生')
+import Child from './views/AboutView.vue';
 
-// 方法
-let name = function () {
-  console.log("调取了1次")
-  return firstName.value + lastName.value
-}
-
-
-// 可读，可修改
-let name2 = computed({
-  get() {
-    return firstName.value + lastName.value;
-  },
-  set(value: string) {
-    console.log("value", value)
-    const [firstName1, lastName1] = value.split("-")  // 数组解构赋值
-    firstName.value = firstName1
-    lastName.value = lastName1
-  },
-});
-
-function handleClickName() {
-  name2.value = "张-三"
-}
 </script>
+
+
+<style>
+.parent {
+  border: 5px solid red;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,21 +1,36 @@
 <!--
  * @Date: 2024-01-19 15:38:25
- * @LastEditTime: 2024-12-11 22:29:37
+ * @LastEditTime: 2024-12-18 00:02:47
  * @Author: wangyongjie
  * @Note:  
 -->
 <template>
   <div class="about">
-    <h1>This is an  page</h1>
+    <input type="text" v-model.number="count1" />
+    <br>
+    <input type="text" v-model.number="count2" />
+    <div>{{ count1 + count2 }}</div>
+
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script lang="ts" setup name="AboutView">
+import { ref, watch } from 'vue'
+
+const count1 = ref(0);
+const count2 = ref(0);
+
+
+const stopWatch = watch(count1, (newVal, oldVal) => {
+  console.log(newVal, oldVal)
+  if (newVal > 10) {
+    stopWatch();
   }
+})
+</script>
+
+<style>
+.about {
+  border: 5px solid blue;
 }
 </style>

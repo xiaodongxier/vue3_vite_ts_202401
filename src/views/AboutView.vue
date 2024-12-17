7028c9c901e8afe8e08e7a30a093c9933da8fa97
wangyongjie <xiaodongxier@gmail.com>
2024-12-18 07:27:51 +0800

    note-sync syncing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2024-01-19 15:38:25
- * @LastEditTime: 2024-12-18 00:02:47
+ * @LastEditTime: 2024-12-18 07:17:30
  * @Author: wangyongjie
  * @Note:  
 -->
 <template>
   <div class="about">
-    <input type="text" v-model.number="count1" />
+    <input type="number" v-model.number="count1" />
     <br>
-    <input type="text" v-model.number="count2" />
+    <input type="number" v-model.number="count2" />
     <div>{{ count1 + count2 }}</div>
 
   </div>
@@ -26,6 +26,9 @@ const stopWatch = watch(count1, (newVal, oldVal) => {
   if (newVal > 10) {
     stopWatch();
   }
+}, {
+  immediate: true,
+  once: true,
 })
 </script>
 

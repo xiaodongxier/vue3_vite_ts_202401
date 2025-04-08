c94e68206d665ef4ec93d40f3e623b4a9c643e0f
wangyongjie <xiaodongxier@gmail.com>
2025-04-09 07:43:11 +0800

    note-sync syncing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
+<!--
+ * @Date: 2025-04-09 07:26:08
+ * @LastEditTime: 2025-04-09 07:33:11
+ * @Author: wangyongjie
+ * @Note:  
+-->
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue';
 </script>
 
 <template>
